ae45026f09d5a1fc2c1c5ce4ed6ca1628fcf33ae
bpbanga <97730466+bpbanga@users.noreply.github.com>
2025-05-29 22:12:57 +0200

    update version of angular

--- src/app/dashboard-component/dashboard-component.component.ts
+++ src/app/dashboard-component/dashboard-component.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard-component',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard-component.component.html',
-  styleUrl: './dashboard-component.component.scss'
+    selector: 'app-dashboard-component',
+    imports: [],
+    templateUrl: './dashboard-component.component.html',
+    styleUrl: './dashboard-component.component.scss'
 })
 export class DashboardComponentComponent {
 
